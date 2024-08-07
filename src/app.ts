75991cb8dfeb20c55933ee6803fc1222fd91b4c6
austin-michael <austin.michaelwill@gmail.com>
2024-08-07 12:50:11 -0600

    Added custom reusable validation for form input fields

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,49 @@
+// Validation for form input fields
+interface Validateable {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+function validate(validateableInput: Validateable) {
+  let isValid = true;
+
+  if (validateableInput.required) {
+    isValid = isValid && validateableInput.value.toString().trim().length !== 0;
+  }
+  if (
+    validateableInput.minLength != null &&
+    typeof validateableInput.value === "string"
+  ) {
+    isValid =
+      isValid && validateableInput.value.length >= validateableInput.minLength;
+  }
+  if (
+    validateableInput.maxLength != null &&
+    typeof validateableInput.value === "string"
+  ) {
+    isValid =
+      isValid && validateableInput.value.length <= validateableInput.maxLength;
+  }
+  if (
+    validateableInput.min != null &&
+    typeof validateableInput.value === "number"
+  ) {
+    isValid = isValid && validateableInput.value >= validateableInput.min;
+  }
+  if (
+    validateableInput.max != null &&
+    typeof validateableInput.value === "number"
+  ) {
+    isValid = isValid && validateableInput.value <= validateableInput.max;
+  }
+
+  return isValid;
+}
+
 // Decorator to prevent having to bind 'this' to various methods
 function autobind(
   _target: any,
@@ -55,14 +101,31 @@ class ProjectInput {
     const enteredDescription = this.descriptionInputElement.value;
     const enteredPeople = this.peopleInputElement.value;
 
+    const titleValidateable: Validateable = {
+      value: enteredTitle,
+      required: true,
+    };
+    const descriptionValidateable: Validateable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5,
+    };
+    const peopleValidateable: Validateable = {
+      value: +enteredPeople,
+      required: true,
+      min: 1,
+      max: 5,
+    };
+
     if (
-      enteredTitle.trim().length === 0 ||
-      enteredDescription.trim().length === 0 ||
-      enteredPeople.trim().length === 0
+      !validate(titleValidateable) ||
+      !validate(descriptionValidateable) ||
+      !validate(peopleValidateable)
     ) {
       alert("Invalid input");
       return;
     } else {
+      console.log(enteredTitle, enteredDescription, enteredPeople);
       return [enteredTitle, enteredDescription, +enteredPeople];
     }
   }
